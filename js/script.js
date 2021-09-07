02e0c18252d462682e393486471d128b82f2b0d7
skrasel143 <skrasel143@gmnail.com>
2021-09-07 19:42:02 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Common cost function
+function extraCost(inputId) {
+    const extraCostText = document.getElementById(inputId);
+    let extraCost = parseFloat(extraCostText.innerText);
+    extraCost = 0;
+    return extraCost;
+}
+
+// Select memory options
+document.getElementById('memory-event').addEventListener('click', function (e) {
+    const memory = e.target.innerText;
+    const lowGb = '8GB unified memory';
+    const highGb = '16GB unified memory';
+    const memoryCost = extraCost('memory-cost');
+    if (memory == lowGb) {
+        document.getElementById('memory-cost').innerText = 0;
+    } else if (memory == highGb) {
+        document.getElementById('memory-cost').innerText = memoryCost + 180;  
+    }
+})
+
+// Select storage options
+document.getElementById('storage-event').addEventListener('click', function (e) {
+    const storage = e.target.innerText;
+    console.log(storage);
+    const lowStorage = '256GB SSD storage';
+    const mediumStorage = '512GB SSD storage';
+    const highStorage = '1TB SSD storage';
+    const storageCostText = document.getElementById('storage-cost');
+    const storageCost = extraCost('storage-cost');
+    if (storage == lowStorage) {
+        storageCostText.innerText = 0;
+    } else if (storage == mediumStorage) {
+        storageCostText.innerText = storageCost + 100;  
+    } else if (storage == highStorage) {
+        storageCostText.innerText = storageCost + 180;
+    }
+})
+
+// Select delivery options
+document.getElementById('delivery-event').addEventListener('click', function (e) {
+    const delivery = e.target.innerText;
+    const lateDelivery = 'Sunday, 12th Sep free prim delivery';
+    const urgentDelivery = 'Friday, 10th Sep delivery charge $20';
+    const deliveryCost = extraCost('delivery-cost');
+    if (delivery == lateDelivery) {
+        document.getElementById('delivery-cost').innerText = 0;
+    } else if (delivery == urgentDelivery) {
+        document.getElementById('delivery-cost').innerText = deliveryCost + 20;  
+    }
+})
